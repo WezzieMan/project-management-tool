40353a5de94a9d454d721b7e01ed82eaf16c9432
Daniel Marcoci <florian.danutz15@gmail.com>
2022-11-23 14:25:13 +0100

    Created project plan by adding all components but empty. Started with components inside layout

---
+++ projectplan/src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
