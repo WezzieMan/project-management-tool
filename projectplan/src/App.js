07906edcf2a009b5c59b97b25c3727570d54dca7
Daniel Marcoci <florian.danutz15@gmail.com>
2022-11-23 16:40:27 +0100

    Added react-router (V6 SO CAREFUL SWITCH IS REPLACED WITH ROUTES). Started with some hard-coded code for dashboard.

--- projectplan/src/App.js
+++ projectplan/src/App.js
@@ -1,13 +1,17 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Dashboard from './components/dashboard/Dashboard';
 import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <div className="App">
         <Navbar />
+        <Routes>
+          <Route path='/' element={<Dashboard/>} />
+        </Routes>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
